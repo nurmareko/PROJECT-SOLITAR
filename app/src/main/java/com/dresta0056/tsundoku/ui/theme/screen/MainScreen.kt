a7b5a403ef6be3076d952a95fadcf359df9de781
nurmareko <nurmareko@yandex.ru>
2026-04-11 18:25:40 +0700

    Update HomeScreen to include a book image

--- app/src/main/java/com/dresta0056/tsundoku/ui/theme/screen/MainScreen.kt
+++ app/src/main/java/com/dresta0056/tsundoku/ui/theme/screen/MainScreen.kt
@@ -1,6 +1,9 @@
 package com.dresta0056.tsundoku.ui.theme.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
@@ -8,15 +11,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.dresta0056.tsundoku.navigation.Screen
 import com.dresta0056.tsundoku.R
+import com.dresta0056.tsundoku.navigation.Screen
 import com.dresta0056.tsundoku.ui.theme.TsundokuTheme
 import com.dresta0056.tsundoku.ui.theme.components.TsundokuTopAppBar
 
@@ -49,10 +54,18 @@ fun MainScreen(
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier,
-        text = "main screen"
-    )
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(top = 20.dp),
+            painter = painterResource(R.drawable.book_stack),
+            contentDescription = stringResource(R.string.stack_of_book_picture)
+        )
+    }
 }
 
 @Preview(showBackground = true)
