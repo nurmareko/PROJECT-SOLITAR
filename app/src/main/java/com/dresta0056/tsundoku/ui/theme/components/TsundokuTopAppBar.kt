e7e89d05b32e74f7773aa73b4b59b938d61fc61f
nurmareko <nurmareko@yandex.ru>
2026-04-12 17:46:25 +0700

    Update TsundokuTopAppBar to use CenterAlignedTopAppBar

--- app/src/main/java/com/dresta0056/tsundoku/ui/theme/components/TsundokuTopAppBar.kt
+++ app/src/main/java/com/dresta0056/tsundoku/ui/theme/components/TsundokuTopAppBar.kt
@@ -1,13 +1,10 @@
 package com.dresta0056.tsundoku.ui.theme.components
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.dresta0056.tsundoku.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,7 +14,7 @@ fun TsundokuTopAppBar(
     actions: @Composable RowScope.() -> Unit = {},
     navigationIcon: @Composable () -> Unit = {}
 ) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         modifier = modifier,
         title = title,
         actions = actions,
