23e952d67fae6defc3c78736df98059808652e29
nurmareko <nurmareko@yandex.ru>
2026-04-12 17:03:12 +0700

    Implement book entry form in AddBookScreen with input fields and category selection

--- app/src/main/java/com/dresta0056/tsundoku/ui/theme/screen/AddBookScreen.kt
+++ app/src/main/java/com/dresta0056/tsundoku/ui/theme/screen/AddBookScreen.kt
@@ -3,19 +3,33 @@ package com.dresta0056.tsundoku.ui.theme.screen
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.dresta0056.tsundoku.R
@@ -51,13 +65,69 @@ fun AddBookScreen(
 fun AddBookContent(
     modifier: Modifier = Modifier
 ) {
+    val radioOptions = listOf("fiction", "non-fiction", "manga", "technical")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
+
+
     Column(
         modifier = modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .selectableGroup(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Text("add a book")
+        OutlinedTextField(
+            value = "Godel, Escher, Bach",
+            onValueChange = { },
+            label = { Text("Book title") },
+            supportingText = { Text("Error") },
+            isError = false,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions()
+        )
+
+        OutlinedTextField(
+            value = "0",
+            onValueChange = { },
+            label = { Text("Number of pages") },
+            trailingIcon = { Text("pages") },
+            supportingText = { Text("Error") },
+            isError = false,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions()
+        )
+
+        radioOptions.forEach { text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = { onOptionSelected(text) },
+                        role = Role.RadioButton
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = null // null recommended for accessibility with screen readers
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+
+        }
+
+        Button(
+            onClick = {}
+        ) {
+            Text("How long will this haunt me?")
+        }
     }
 }
 
