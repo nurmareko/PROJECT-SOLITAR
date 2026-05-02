004a6d570396d5d827557d8bef502b737b778e16
nurmareko <nurmareko@yandex.ru>
2026-05-02 23:20:35 +0700

    Add Room, DataStore, and KSP support and update dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -14,6 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.dresta0056.tsundoku"
         minSdk = 26
+        //noinspection OldTargetApi
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -50,8 +52,14 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.compose.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.10.6")
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.compose.runtime.saveable)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
